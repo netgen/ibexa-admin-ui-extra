fcce0719feac87b0d7786ebe993558512db0d777
Petar Jakopec <petar.jakopec0@gmail.com>
2025-07-31 13:42:13 +0200

    NGSTACK-983 update bundle class

--- bundle/NetgenIbexaAdminUIExtraBundle.php
+++ bundle/NetgenIbexaAdminUIExtraBundle.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\IbexaAdminUIExtraBundle;
 
+use Ibexa\Bundle\Core\DependencyInjection\IbexaCoreExtension;
+use Netgen\Bundle\IbexaAdminUIExtraBundle\DependencyInjection\Compiler\DisableQueuesPass;
 use Netgen\Bundle\IbexaAdminUIExtraBundle\DependencyInjection\Compiler\SearchOverridePass;
+use Netgen\Bundle\IbexaAdminUIExtraBundle\DependencyInjection\Security\PolicyProvider\QueuesPolicyProvider;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -13,5 +16,10 @@ final class NetgenIbexaAdminUIExtraBundle extends Bundle
     public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new SearchOverridePass());
+        $container->addCompilerPass(new DisableQueuesPass());
+
+        /** @var IbexaCoreExtension $ibexaCoreExtension */
+        $ibexaCoreExtension = $container->getExtension('ibexa');
+        $ibexaCoreExtension->addPolicyProvider(new QueuesPolicyProvider());
     }
 }
