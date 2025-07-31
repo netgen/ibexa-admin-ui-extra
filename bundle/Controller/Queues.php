9ce8c8029984256d2820bc3bf000a066ee63b8c0
Petar Jakopec <petar.jakopec0@gmail.com>
2025-07-31 13:40:37 +0200

    NGSTACK-983 implement queues module in administration

---
+++ bundle/Controller/Queues.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\IbexaAdminUIExtraBundle\Controller;
+
+use Ibexa\Contracts\AdminUi\Controller\Controller;
+use Ibexa\Contracts\Core\Repository\PermissionResolver;
+use Symfony\Component\DependencyInjection\ServiceLocator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Messenger\Transport\Receiver\MessageCountAwareInterface;
+
+use function array_keys;
+use function strcmp;
+use function usort;
+
+final class Queues extends Controller
+{
+    public function __construct(
+        public readonly ServiceLocator $transportLocator,
+        private readonly bool $queuesDisabled,
+        private readonly array $allowedTransports,
+        private readonly PermissionResolver $permissionResolver,
+    ) {}
+
+    public function __invoke(): Response
+    {
+        if ($this->queuesDisabled) {
+            throw new NotFoundHttpException('Queues feature is disabled.');
+        }
+
+        if (!$this->permissionResolver->hasAccess('queues', 'read')) {
+            throw new AccessDeniedHttpException('You do not have permission to access queues.');
+        }
+
+        $transportNames = !empty($this->allowedTransports)
+            ? $this->allowedTransports
+            : array_keys($this->transportLocator->getProvidedServices());
+
+        $queueData = [];
+
+        foreach ($transportNames as $transportName) {
+            if (!$this->transportLocator->has($transportName)) {
+                continue;
+            }
+
+            $transport = $this->transportLocator->get($transportName);
+
+            if (!$transport instanceof MessageCountAwareInterface) {
+                continue;
+            }
+
+            $queueData[] = [
+                'name' => $transportName,
+                'count' => $transport->getMessageCount(),
+            ];
+        }
+
+        usort($queueData, static fn (array $first, array $second) => strcmp($first['name'], $second['name']));
+
+        return $this->render(
+            '@NetgenIbexaAdminUIExtra/queues/list.html.twig',
+            [
+                'translation_domain' => 'netgen_admin_ui_extra',
+                'queue_data' => $queueData,
+            ],
+        );
+    }
+}
